76739dcf222733f3ed9af5afc38446501c262943
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2022-11-15 15:44:42 -0300

    ADD - Documentation of the Functions and Change the "href" Attribute According the Active Course in the Slider

--- Site/assets/js/index.js
+++ Site/assets/js/index.js
@@ -1,45 +1,74 @@
-// GLOBAL VARIABLES
-// Opening and Closing Menu
-let menuIsActive = false;
-
-// Access Course Button
-let coursesLink = [
-    "Canto",
-    "Teclado",
-    "Piano",
-    "Musicalização Infantil",
-    "Regente Coral"
-];
-
 (function() {
-    // Opening and Closing Menu
-    let $menuHamburguerButton = document.querySelector(".menu__hamburguer-icon");
-    $menuHamburguerButton.addEventListener("click", function() {
-        menuIsActive = !menuIsActive;
+    /* OPENING AND CLOSING MENU *
+     * 
+     * GOAL:
+     * Close and Open the Menu if the Hamburguer menu is clicked
+     * 
+     * STRATEGY:
+     * Use a boolean variable to check if the menu is active and customize the element's CSS according to the boolean
+     */
+    {
+        let menuIsActive = false;
 
-        let $menu = document.querySelector(".menu");
-        let $navigationMenu = document.querySelector(".navigation-menu");
-        if(menuIsActive) {
-            $menu.style.backgroundColor = "var(--menu-background)";
-            $menu.style.boxShadow = "-2px 2px 4px rgba(0, 0, 0, .5)";
-            
-            $menuHamburguerButton.src = "assets/images/icons/Close.svg";
-            
-            $navigationMenu.style.height = "auto";
-        } else {
-            $menu.style.backgroundColor = "transparent";
-            $menu.style.boxShadow = "none";
-            
-            $menuHamburguerButton.src = "assets/images/icons/Menu.svg";
+        let $menuHamburguerButton = document.querySelector(".menu__hamburguer-icon");
+        $menuHamburguerButton.addEventListener("click", function() {
+            menuIsActive = !menuIsActive;
 
-            $navigationMenu.style.height = 0;
-        }
-    });
+            let $menu = document.querySelector(".menu");
+            let $navigationMenu = document.querySelector(".navigation-menu");
+            // Change the CSS according the "menuIsActive"
+            if(menuIsActive) {
+                $menu.style.backgroundColor = "var(--menu-background)";
+                $menu.style.boxShadow = "-2px 2px 4px rgba(0, 0, 0, .5)";
+                
+                $menuHamburguerButton.src = "assets/images/icons/Close.svg";
+                
+                $navigationMenu.style.height = "auto";
+            } else {
+                $menu.style.backgroundColor = "transparent";
+                $menu.style.boxShadow = "none";
+                
+                $menuHamburguerButton.src = "assets/images/icons/Menu.svg";
 
-    // Access Course Button
-    let $carouselBullets = document.querySelectorAll(".swiper-pagination-bullet");
+                $navigationMenu.style.height = 0;
+            }
+        });
+    }
 
-    let $accessCourseButton = document.querySelector(".courses__acess-course");
+    /* ACCESS COURSE BUTTON *
+     * 
+     * GOAL:
+     * Change the "src" attribute value according the active slide of the courses
+     * 
+     * STRATEGY:
+     * Get the Bullets Pagination, and get the bullet with class "swiper-pagination-bullet-active", and according the index, set the "href" of the link with the value of the array of courses
+     * To do this, the MutationObserver is used to listening the changes in the "swiper-pagination" element
+     */
+    {
+        //TODO: Add the URL courses
+        let coursesLink = [
+            "Canto",
+            "Teclado",
+            "Piano",
+            "Musicalização Infantil",
+            "Regente Coral"
+        ];
 
-    
+        let $carouselBullets = document.querySelector(".swiper-pagination");
+     
+        let $accessCourseButton = document.querySelector(".courses__acess-course");
+        let mutationObserver = new MutationObserver(() => {
+            $carouselBullets.childNodes.forEach((bullet, index) => {
+                if(bullet.classList.contains("swiper-pagination-bullet-active")) {
+                    // Set the "href" of the "<a></a>" tag according the index of the "coursesLink"
+                    $accessCourseButton.href = coursesLink[index];
+                }
+            });
+        });
+        
+        mutationObserver.observe($carouselBullets, {
+            attributeFilter: ["class"],
+            subtree: true
+        });
+    }
 })();
