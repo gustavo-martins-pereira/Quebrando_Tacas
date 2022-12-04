d9f5252c7f9f235a6f2a3092c783498b460d3e4e
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2022-12-03 21:31:37 -0300

    MOD - Less Complex Hamburguer Menu Logic

--- Site/assets/js/hamburguerMenu.js
+++ Site/assets/js/hamburguerMenu.js
@@ -5,50 +5,21 @@
      * Close and Open the Menu if the Hamburguer menu is clicked
      * 
      * STRATEGY:
-     * Use a boolean variable to check if the menu is active and customize the element's CSS according to the boolean
+     * Add or Remove the class "menu-is-active" to apply the CSS styles
      */
     {
-        let menuIsActive = false;
-
         let $menu = document.querySelector("[data-element-id='menu']");
-        let $navigationMenu = document.querySelector("[data-element-id='navigationMenu']");
         let $menuHamburguerButton = document.querySelector("[data-element-id='menuHamburguerIcon']");
-        
-        let assetsPath = $menuHamburguerButton.dataset.assetsPath;
-        
+
         $menuHamburguerButton.addEventListener("click", function() {
-            menuIsActive = !menuIsActive;
-
-            // Change the CSS according the "menuIsActive"
-            if(menuIsActive) {
-                // TODO: Change the color to a variable name
-                $menu.style.backgroundColor = "hsl(202, 100%, 71%)";
-                $menu.style.width = "auto";
-                $menu.style.boxShadow = "-2px 2px 4px hsla(0, 0%, 0%, 0.8)";
-                
-                $menuHamburguerButton.src = `${assetsPath}/images/icons/Close.svg`;
-                
-                $navigationMenu.style.height = "auto";
-            } else {
-                $menu.style.backgroundColor = "transparent";
-                $menu.style.width = "75px";
-                $menu.style.boxShadow = "none";
-                
-                $menuHamburguerButton.src = `${assetsPath}/images/icons/Menu.svg`;
-
-                $navigationMenu.style.height = 0;
-            }
+            $menu.classList.toggle("menu-is-active");
         });
 
         // Fix the bug when screen changes size with icon open or closed
         document.body.onresize = function() {
             let clientWindowTarget = 1024;
             if(document.body.clientWidth >= clientWindowTarget - 17) {
-                $menu.removeAttribute("style");
-                $menuHamburguerButton.src = `${assetsPath}/images/icons/Menu.svg`;
-                $navigationMenu.removeAttribute("style");
-
-                menuIsActive = false;
+                $menu.classList.remove("menu-is-active");
             }
         }
     }
