828fe29815b7c6d472a03afa08f5f7cd51d5921c
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2022-12-19 20:24:12 -0300

    MOD - Change the Function to Identify Any Elements in the Node with "data-dropdown-content"

--- Site/assets/js/dropDown.js
+++ Site/assets/js/dropDown.js
@@ -1,25 +1,39 @@
 (function() {
     /* DROP DOWN THE CONTENT BOX *
+     *
      * GOAL:
      * Drop down a content's box when there is clicked
      * 
      * STRATEGY:
-     * Get the elements with "data-dropdown-trigger" attribute and get the brothers with a "data-dropdown-content" attribute and show it
+     * Get the elements with "data-dropdown-trigger" attribute in the page and get the elements inside this parent with a "data-dropdown-content" attribute and put a class called "active"
+     * The functionality performs a recursive function going through the children and checking which ones contain the indicated attribute
+     * 
+     * COMMENTS:
+     * Define the style of the "active" class on the parent element of the element that contains the "data-dropdown-trigger" attribute
     */
     {
         let $dropdownTriggers = document.querySelectorAll("[data-dropdown-trigger]");
 
         $dropdownTriggers.forEach(dropdownTrigger => {
             dropdownTrigger.addEventListener("click", () => {
-                // TODO: Get the content by the full node of the children
                 let parentNode = dropdownTrigger.parentElement;
 
-                for (let child of parentNode.children) {
-                    if(child.hasAttribute("data-dropdown-content")) {
-                        parentNode.classList.toggle("active");
-                    }
-                }
+                activeDropdownContent(parentNode, parentNode.children);
             });
         });
+
+        function activeDropdownContent(mainParentNode, children) {
+            for (const child of children) {
+                let hasDataDropdownContent = child.hasAttribute("data-dropdown-content");
+
+                if(child.children && !hasDataDropdownContent) {
+                    activeDropdownContent(mainParentNode, child.children);
+                } else {
+                    if(hasDataDropdownContent) {
+                        mainParentNode.classList.toggle("active");
+                    }
+                }
+            }
+        }
     }
 })();
