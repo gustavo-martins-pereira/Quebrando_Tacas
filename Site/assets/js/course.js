1d02e9aa3f9a90438a52bbd26694b826ce65a1d0
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2023-01-04 13:01:43 -0300

    ADD - QR Code in All Courses and Show Content when Button is Clicked

--- Site/assets/js/course.js
+++ Site/assets/js/course.js
@@ -1,9 +1,19 @@
 (function() {
+    /* SHOW QR CODE TO BUY A COURSE *
+     * 
+     * GOAL:
+     * Appear a QR Code when the "Compre agora" Button is clicked
+     * 
+     * STRATEGY:
+     * Remove the class which hidde the QR Code content
+     */
     {
-        const $buyCourseButton = document.querySelector("[data-element-id='buy-course']");
+        const $buyCourseButton = document.querySelector("[data-element-id='buy-course-button']");
 
         $buyCourseButton.addEventListener("click", () => {
-            console.log();
+            const $buyCourseContent = document.querySelector("[data-element-id='buy-course-content']");
+
+            $buyCourseContent.classList.remove("price__payment--hidden");
         });
     }
 })();
