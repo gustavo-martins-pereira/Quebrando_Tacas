1a1199e9dc1d95eef94ea9fcbf139cfb19695999
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2023-01-04 11:45:39 -0300

    FIX - Load Sections in Pages Without Button "Load Other Section"

--- Site/assets/js/dropDownContent.js
+++ Site/assets/js/dropDownContent.js
@@ -38,31 +38,4 @@
             }
         }
     }
-
-    /* DROP DOWN THE CONTENT BOX *
-     *
-     * GOAL:
-     * Drop down a content's box when there is clicked
-     * 
-     * STRATEGY:
-     * Toggle a class which set the max-height when the button is clicked
-    */
-    {
-        let $loadOtherSectionsButton = document.querySelector("[data-element-id='loadOtherSectionsButton']");
-
-        let allSectionsIsLoaded = false;
-        $loadOtherSectionsButton.addEventListener("click", () => {
-            let $sections = document.querySelector("[data-element-id='sections']");
-
-            $sections.classList.toggle("learning__sections--max-height");
-
-            allSectionsIsLoaded = !allSectionsIsLoaded;
-            
-            if(allSectionsIsLoaded) {
-                $loadOtherSectionsButton.innerText = "Esconder as outras seções";
-            } else {
-                $loadOtherSectionsButton.innerText = "Carregar as Outras Seções";
-            }
-        });
-    }
 })();
