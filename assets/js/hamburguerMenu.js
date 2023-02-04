fe24893b15b27c17dd7aad5fd13da78454810dc4
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2023-02-04 12:53:25 -0300

    ALL - Project in the Folder Root and NodeJS Included in the Project

---
+++ assets/js/hamburguerMenu.js
@@ -0,0 +1,31 @@
+/* OPENING AND CLOSING MENU *
+ * 
+ * GOAL:
+ * Close and Open the Menu if the Hamburguer menu is clicked
+ * 
+ * STRATEGY:
+ * Add or Remove the class "menu-is-active" to apply the CSS styles
+ */
+export default function makeMenuLogic() {
+    const $menu = document.querySelector("[data-element-id='menu']");
+    const $menuHamburguerButton = document.querySelector("[data-element-id='menuHamburguerIcon']");
+
+    $menuHamburguerButton.addEventListener("click", function () {
+        $menu.classList.toggle("menu-is-active");
+    });
+
+    // Fix the bug when screen changes size with icon open or closed
+    document.body.onresize = function () {
+        const clientWindowTarget = 1024;
+        if (document.body.clientWidth >= clientWindowTarget - 17) {
+            $menu.classList.remove("menu-is-active");
+        }
+    }
+
+    // Close menu when click outside screen
+    window.addEventListener("click", event => {
+        if (!event.composedPath().includes($menu)) {
+            $menu.classList.remove("menu-is-active");
+        }
+    });
+}
