9936a29598246fed8ae20adfc8a5d609ee3c4e5a
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2023-02-26 16:40:02 -0300

    ADD - Loading CSS Files with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,22 @@ const path = require('path');
 
 module.exports = {
     entry: './app/src/assets/js/pages/index.js',
-  
+
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, 'app/dist'),
+        path: path.resolve(__dirname, 'app/dist/assets/js'),
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
     },
 };
