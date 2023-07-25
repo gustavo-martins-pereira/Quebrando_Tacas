3683e5284aab3de78a5655b5a6084d6258744d8d
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2023-07-25 11:59:57 -0300

    A&R - Classes Content and Frequent Questions Content & Accordion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-const $classesCourseItens = document.querySelectorAll("[data-element-id='classes-course__item']");
-$classesCourseItens.forEach($classesCourseItem => {
-    $classesCourseItem.addEventListener("click", () => {
-        $classesCourseItem.classList.toggle("classes-course__item--active");
-    });
-});
+// const $classesCourseItens = document.querySelectorAll("[data-element-id='classes-course__item']");
+// $classesCourseItens.forEach($classesCourseItem => {
+//     $classesCourseItem.addEventListener("click", () => {
+//         $classesCourseItem.classList.toggle("classes-course__item--active");
+//     });
+// });
