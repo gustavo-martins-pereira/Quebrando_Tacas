aac62375497f52a99b9bbc886c0c7f3206e83940
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2023-12-30 14:50:07 -0300

    M&D - Preview Share Link & Unnused JS

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-// const $classesCourseItens = document.querySelectorAll("[data-element-id='classes-course__item']");
-// $classesCourseItens.forEach($classesCourseItem => {
-//     $classesCourseItem.addEventListener("click", () => {
-//         $classesCourseItem.classList.toggle("classes-course__item--active");
-//     });
-// });
